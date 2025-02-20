2167c348d555bd8e7d8b9b38c156b64e4298d8cf
Song Khoi <songkhoi123@gmail.com>
2025-02-20 23:50:34 +0700

    fix route

--- src/banking_on_clojure/core.clj
+++ src/banking_on_clojure/core.clj
@@ -1,33 +1,49 @@
 (ns banking-on-clojure.core
   (:gen-class)
-  (:require [io.pedestal.http :as http]
-            [reitit.core :as r]
+  (:require [io.pedestal.http :as server]
+            [reitit.pedestal :as pedestal]
+            [reitit.http :as http]
+            [reitit.ring :as ring]
             [reitit.dev.pretty :as pretty]
             [environ.core :refer [env]]))
 
 ;; our first handler
+(defn interceptor [number]
+  {:enter (fn [ctx] (update-in ctx [:request :number] (fnil + 0) number))})
+
 (defn hello
   [req]
   {:body "Hello world!"
    :status 200})
 
 (def routes
-  (r/router
-   [["/api/ping" ::ping]
-    ["/api/orders/:id" ::order]]))
+  ["/api"
+   {:interceptors [(interceptor 1)]}
+   ["/api/ping" ::ping]
+   ["/api/orders/:id" ::order]
+   ["/number"
+    {:interceptors [(interceptor 10)]
+     :get {:interceptors [(interceptor 100)]
+           :handler (fn [req]
+                      {:status 200
+                       :body (select-keys req [:number])})}}]])
 
 (defonce http-state (atom nil))
 (defn dev-start
   [& _]
   (swap! http-state (fn [st]
                       ;; if there is something running, stop it
-                      (some-> st http/stop)
-                      (-> {::http/routes routes
-                           ::http/host   "0.0.0.0"
-                           ::http/port   (Integer. (or (env :port) 5000))
-                           ::http/join?  false
-                           ::http/type   :jetty}
-                          http/default-interceptors
-                          http/dev-interceptors
-                          http/create-server
-                          http/start))))
+                      (some-> st server/stop)
+                      (-> {::server/host   "0.0.0.0"
+                           ::server/port   (Integer. (or (env :port) 5000))
+                           ::server/join?  false
+                           ::server/type   :jetty
+                           ::server/routes []}
+                          (server/default-interceptors)
+                              ;; swap the reitit router
+                          (pedestal/replace-last-interceptor
+                           (pedestal/routing-interceptor
+                            (http/router routes)))
+                          (server/dev-interceptors)
+                          (server/create-server)
+                          (server/start)))))
