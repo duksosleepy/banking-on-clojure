199191a8d9db11afad89a07498a90ee25d151395
Song Khoi <songkhoi123@gmail.com>
2025-02-20 21:37:02 +0700

    define route

--- src/banking_on_clojure/core.clj
+++ src/banking_on_clojure/core.clj
@@ -1,11 +1,33 @@
 (ns banking-on-clojure.core
-  (:gen-class))
+  (:gen-class)
+  (:require [io.pedestal.http :as http]
+            [reitit.core :as r]
+            [reitit.dev.pretty :as pretty]
+            [environ.core :refer [env]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; our first handler
+(defn hello
+  [req]
+  {:body "Hello world!"
+   :status 200})
 
-(defn -main []
-  (println "Server starting...")
-  (foo "Song Khoi say: "))
+(def routes
+  (r/router
+   [["/api/ping" ::ping]
+    ["/api/orders/:id" ::order]]))
+
+(defonce http-state (atom nil))
+(defn dev-start
+  [& _]
+  (swap! http-state (fn [st]
+                      ;; if there is something running, stop it
+                      (some-> st http/stop)
+                      (-> {::http/routes routes
+                           ::http/host   "0.0.0.0"
+                           ::http/port   (Integer. (or (env :port) 5000))
+                           ::http/join?  false
+                           ::http/type   :jetty}
+                          http/default-interceptors
+                          http/dev-interceptors
+                          http/create-server
+                          http/start))))
